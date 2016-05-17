dcbe9c963221f7016381b7919f1823436170e2e9
eledata <huangmoyue@163.com>
2016-05-17 14:15:12 +0800

    update
    update the files

---
+++ R Stats and Model/Data_Description.R
@@ -0,0 +1,136 @@
+# Data Description
+# Test Data
+x <- c(74.3,78.8,68.8,78.0,70.4,80.5,80.5,69.7,71.2,73.5,
+79.5,75.6,75.0,78.8,72.0,72.0,72.0,74.3,71.2,72.0,
+75.0,73.5,78.8,74.3,75.8,65.0,74.3,71.2,69.7,68.0,
+73.5,75.0,72.0,64.3,75.8,80.3,69.7,74.3,73.5,73.5,
+75.8,75.8,68.8,76.5,70.4,71.2,81.2,75.0,70.4,68.0,
+70.4,72.0,76.5,74.3,76.5,77.6,67.3,72.0,75.0,74.3,
+73.5,79.5,73.5,74.7,65.0,76.5,81.6,75.4,72.7,72.7,
+67.2,76.5,72.7,70.4,77.2,68.8,67.3,67.3,67.3,72.7,
+75.8,73.5,75.0,73.5,73.5,73.5,72.7,81.6,70.3,74.3,
+73.5,79.5,70.4,76.5,72.7,77.2,84.3,75.0,76.5,70.4)
+
+# Data source
+df_people_info <- data.frame(Name = c("A", "B", "C", "D", "E"),
+                             Sex = c("F", "M", "F", "F", "M"),
+                             Age = c(16, 27, 28, 20, 21),
+                             Height = c(180, 177, 189, 168, 172),
+                             Weight = c(76, 87, 69, 79, 70)
+                             )
+df_people_info
+
+incomes <- c(60, 49, 40, 61, 64, 60, 59, 54, 62, 69, 70, 42, 56,
+             61, 61, 61, 58, 51, 48, 65, 49, 49, 41, 48, 52, 46,
+             59, 46, 58, 43)
+incomes
+
+# 位置的度量
+# mean sort quantile median
+# 分散程度的度量&分布形状的度量
+# 方差，变异系数， 极差， 标准误，偏度系数，峰度系数
+
+# mean - 均值 v - 样本方差  s - 标准差 me - 中位数
+# cv - 变异系数 css - 样本校正平方差 uss - 样本未校正平方和
+# R - 极差 R1 - 四分位差 sm - 样本标准差 g1 - 偏度系数
+# g2 - 峰度系数
+
+data_outline <- function(x){
+  n <- length(x)
+  m <- mean(x)
+  v <- var(x)
+  s <- sd(x)
+  me <- median(x)
+  cv <- 100*s/m
+  css <- sum((x - m)^2)
+  uss <- sum(x^2)
+  R <- max(x) - min(x)
+  R1 <- quantile(x, 3/4) - quantile(x, 1/4)
+  sm <- s/sqrt(n)
+  g1 <- (n/((n-1)*(n-2)*s^3))*((sum(x-m)^3)/n)
+  g2 <- ((n*n*(n+1))/((n-1)*(n-2)*(n-3)*(s^4)))*(sum(x-m)^4/n)-(3*(n-1)^2)/((n-2)*(n-3))
+  data.frame(
+    N = n, Mean = m, Var = v, SD = s, Median = me, CV = cv, CSS = css, USS = uss, R = R,
+    R1 = R1, StandardMean = sm, Skewness = g1, Kurtosis = g2, row.names =  1
+  )
+  }
+data_outline(x)
+
+#hist 直方图 默认的是频率直方图
+p_data <- x
+
+hist(p_data)
+# 密度直方图
+hist(p_data, freq = FALSE)
+#密度估计函数
+density(p_data)
+lines(density(p_data), col = "red")
+
+#经验分布函数,可以估计样本总体的分布函数F(X)
+ecdf(p_data)
+plot(ecdf(p_data), verticals = TRUE, do.p = FALSE)
+x <- 60:90
+lines(x, pnorm(x, mean(p_data), sd(p_data)))
+
+#QQ 图可以帮我们鉴别样本的分布是否近似于某种类型的分布
+qqnorm(p_data)
+qqline(p_data)
+
+#茎叶图， 看出数据的分布情况
+stem(p_data)
+
+#箱线图， 展现数据分布的主要特征
+boxplot(p_data)
+
+#5数总括图 min, Q1, mid, Q3, max
+fivenum(p_data)
+
+#正态性W检验
+p_data <- scan("Problem_3.1.data")
+shapiro.test(p_data)
+
+#经验分布的K-S检验方法， 检验经验分布和假设总体分布函数之间的差异
+ks.test(p_data)
+
+
+# 相关性检验
+
+p_data <- data.frame(
+  x <- c(2,4,3,2,4,7,7,2,2,5,4,3),
+  y <- c(5,6,8,5,10,7,12,12,6,6,6,8),
+  z <- c(7,11,6,6,7,9,5,5,10,6,3,10)
+)
+p_data
+# Step 1: calculate the mean
+p_data.mx <- mean(p_data$x)
+p_data.mx
+p_data.my <- mean(p_data$y)
+p_data.my
+p_data.mz <- mean(p_data$z)
+p_data.mz
+
+
+# Step 2: Calculate the cov&cor
+p_data.v <- cov(p_data)
+p_data.v
+p_data.r <- cor(p_data)
+p_data.r
+
+# Step 3: Using the cor.test to verify the sample's cor and tot's cor
+cor.test(~x + y, data = p_data)
+cor.test(~x + z, data = p_data)
+cor.test(~y + z, data = p_data)
+
+
+p_data <- data.frame(
+  x <- c(2,4,3,2,4,7,7,2,2,5,4,NaN),
+  y <- c(5,6,8,5,10,7,12,12,6,6,NaN,NaN),
+  z <- c(7,11,6,6,7,9,5,5,10,6,3,10)
+)
+plot(p_data)
+boxplot(p_data)
+plot(z)
+
+
+
+
--- R Stats and Model/Hypothesis_Test.R
+++ R Stats and Model/Hypothesis_Test.R
@@ -1,3 +1,7 @@
+# Hypothesis Test
+
+# 1. For important value test, like mean, sigma.
+
 #P Value Calculation Method
 P_Value <- function(cdf, x, paramet = numeric(0), side = 0){
   n <- length(paramet)
@@ -7,9 +11,10 @@ P_Value <- function(cdf, x, paramet = numeric(0), side = 0){
     cdf(x, paramet[1], paramet[2]),
     cdf(x, paramet[1], paramet[2], paramet[3])
   )
-  if (side < 0) P
-  else if (side > 0) 1 - P
+  if (side < 0) P # Left side
+  else if (side > 0) 1 - P # Right side
   else 
+    # Two sides, choose lower value.
     if(P < 0.5) 2*P
     else 2*(1 - P)
 }
@@ -38,18 +43,18 @@ mean_test2 <- function(x, y, var.equal = FALSE, sigma = c(-1, -1), side = 0){
   mx <- mean(x)
   my <- mean(y)
   if(all(sigma > 0)){
-    z <- (mx - my)/(sqrt(sigma[1]^2/nx + sigma[2]^2/ny))
+    z <- (mx - my)/(sqrt(sigma[1]^2/nx + sigma[2]^2/ny)) # Z 
     P <- P_Value(pnorm, z, side = side)
     data.frame(mean = mx - my, df = nx + ny, Z = z, P_Value = P)
   }else{
     if(var.equal == TRUE){
-      sw <- sqrt(((nx - 1)*sd(x)^2 + (ny - 1)*sd(x)^2)/(nx + ny - 2))
+      sw <- sqrt(((nx - 1)*sd(x)^2 + (ny - 1)*sd(x)^2)/(nx + ny - 2)) #SW
       t <- (mx - my)/(sw*(sqrt(1/nx + 1/ny)))
       nu <- nx + ny - 2
     }else{
         s1 <- var(x)
         s2 <- var(y)
-        nu <- (s1/nx + s2/ny)^2/(s1^2/(nx^2*(nx - 1)) + s2^2/ny^2*(ny - 1))
+        nu <- (s1/nx + s2/ny)^2/(s1^2/(nx^2*(nx - 1)) + s2^2/ny^2*(ny - 1)) # NY
         t <- (mx - my)/sqrt(s1/nx + s2/ny)
       }
     P <- P_Value(pt, t, nu, side = side)
@@ -63,7 +68,9 @@ Y<-c(79.1, 81.0, 77.3, 79.1, 80.0, 79.1, 79.1, 77.3, 80.2, 82.1)
 mean_test2(X,Y, var.equal=TRUE, side=-1)
 mean_test2(X,Y, side=-1)
 
+# Standard Deviation Test
 
+# 1. Sigle
 var_test1 <- function(x, sigma = 1, mu = Inf, side = 0){
   n <- length(x)
   if(mu < Inf){
@@ -79,6 +86,7 @@ var_test1 <- function(x, sigma = 1, mu = Inf, side = 0){
 }
 var_test1(X, sigma = 3.5)
 
+# 2. Double
 var_test2 <- function(x, y, mu = c(Inf, Inf), side = 0){
   nx <- length(x)
   ny <- length(y)
@@ -100,11 +108,11 @@ var_test2 <- function(x, y, mu = c(Inf, Inf), side = 0){
 
 var_test2(X,Y)
 
-#pearson 拟合优度卡方检验
+#pearson 拟合优度卡方检�?
 # 1. 检验样本是否符合某分布
 
 
-#判断某个总体的不同的水平下是否存在显著差异。
+#判断某个总体的不同的水平下是否存在显著差异�?
 X <- c(210, 312, 170, 85, 223)
 n <- sum(X)
 m <- length(X)
@@ -124,23 +132,24 @@ p[n] <- 1 - q[n-1]
 for(i in 2:(n - 1)){
   p[i] <- q[i] - q[i - 1]
 }
-
+p
 chisq.test(Y,p=p)
 
 X <- 0:4
 Y <- c(7, 10, 12, 8, 5)
 q <- ppois(X, mean(rep(X,Y)))
 n <- length(Y)
+p <- rep(NA, n)
 p[1] <- q[1]
 p[n] <- 1 - q[n-1]
-for(i in 2:(n - 1)){
+for(i in 2:(n-1)){
   p[i] <- q[i] - q[i - 1]
 }
-
+p
 chisq.test(Y,p=p)
 
 
-#列联表
+#列联�?
 X <- data.frame(
   a1 <- c(20, 24, 80, 82),
   a2 <- c(22, 38, 104, 125),
@@ -152,15 +161,80 @@ X
 as.matrix(X, nrow = 4, ncol = 4)
 chisq.test(X)
 
-#Fisher 精确独立检验
+#Fisher 精确独立检�?
 x <- c(4,5,18,6)
 dim(x) <- c(2,2)
 fisher.test(x)
 
-#spearman
+# spearman&kendall correlation test, allow non-normal distribution collrelation test.
 x <- c(1,2,3,4,5,6)
 y <- c(6,5,4,3,2,1)
 cor.test(x,y,method = "spearman")
+cor.test(x,y,method = "kendall")
 
+# Home Work
 
+# 1
 
+blood <- c(220,188,162,230,145,160,238,188,247,113,126,245,164,231,256,183,190,158,224,175)
+mu <- 225
+t.test(blood, mu = mu)
+
+# 2
+
+x<-c(1067,919,1196,785,1126,936,918,1156,920,948)
+p1000 <- 1 - pnorm(1000, mean = mean(x), sd = sd(x))
+p1000
+
+# 3
+
+x<-c(113,120,138,120,100,118,138,123)
+y<-c(138,116,125,136,110,132,130,110) 
+wilcox.test(x,y)
+
+test.data <- data.frame(
+  X = c(113,120,138,120,100,118,138,123,138,116,125,136,110,132,130,110),
+  A = gl(2,8,16)
+  )
+test.data
+test.aov <- aov(X~A, data = test.data)
+summary(test.aov)
+
+# 4
+
+x<-c(-0.7,-5.6,2,2.8,0.7,3.5,4,5.8,7.1,-0.5,2.5,-1.6,1.7,3,0.4,4.5,4.6,2.5,6,-1.4)
+y<-c(3.7,6.5,5,5.2,0.8,0.2,0.6,3.4,6.6,-1.1,6,3.8,2,1.6,2,2.2,1.2,3.1,1.7,-2)
+
+# Distribution test
+shapiro.test(x)
+shapiro.test(y)
+
+# KS test
+ks.test(x, pnorm, mean(x), sd(x))
+ks.test(y, pnorm, mean(y), sd(y))
+
+# Pearson Test
+A <- table(cut(x, br = c(-2,2,4,8)))
+p <- pnorm(c(-2,2,4,8), mean(x), sd(x))
+p <- c(p[2],p[3]- p[2],1 - p[3])
+chisq.test(A, p = p)
+
+# Self function
+mean_test2(x,y, var.equal=TRUE, side=-1)
+mean_test2(x,y, side=-1)
+var_test2(x,y)
+
+# Sys Internal function
+t.test(x,y,var.equal=T)
+t.test(x,y,var.equal=F)
+var.test(x,y)
+
+# 5
+x<-0:5
+y<-c(92,68,28,11,1,0) 
+z <- rep(x,y)
+B <- table(cut(z, br = c(-1,0,1,2,5))) # Step 1, Split into small interval
+B
+q<-ppois(c(0,1,2,5),mean(z))  # Step 2, fill the vaule into the small interval
+p<-c(q[1],q[2]-q[1],q[3]-q[2],1-q[3]) # Step 3, calculate the value for interval
+chisq.test(B,p=p)
