08e1fb55f0597624131f2221bb6be2dd58b9b0cb
Marlon Huang Moyue <marlon.huang@GLOBAL.ECOLAB.CORP>
2017-05-08 18:44:24 +0800

    updated
    updated!

--- Python-data-analysis/Text_clf/sougou_news_clf.py
+++ Python-data-analysis/Text_clf/sougou_news_clf.py
@@ -5,7 +5,40 @@ Created on Mon May 08 15:59:02 2017
 @author: HUANGMA
 """
 
+import os
+import sys
+from sklearn import naive_bayes
+from bs4 import BeautifulSoup
+import pandas as pd
 import jieba
+import chardet
 
-seg_list = jieba.cut("我来到北京清华大学", cut_all=True)
-print("Full Mode: " + "/ ".join(seg_list))  # 全模式
+path = os.getcwd()
+datapath = path + '\\SogouCS\\'
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+link = []
+webdetails = []
+filelist = os.listdir(datapath)
+
+
+for fl in filelist:
+    link.append(datapath + fl) 
+
+for link_item in link:
+    tmp = open(link_item)
+    line = tmp.read()
+    i = 0
+    while i <= len(line) - 6:
+        webdetails.append(line[i:i+5])
+        i = i + 6
+    print line
+        
+
+
+        
+        
+tmp.close()
+
